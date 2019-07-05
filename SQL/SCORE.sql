02041cb3af427d5858d97badc2bf18286612e039
kseymin <37235453+kseymin@users.noreply.github.com>
2019-07-05 14:42:05 +0900

    Add SQL schema files

---
+++ SQL/SCORE.sql
@@ -0,0 +1,36 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-7월-05-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table SCORE
+--------------------------------------------------------
+
+  CREATE TABLE "ORA_USER"."SCORE" 
+   (	"USER_ID" VARCHAR2(20 BYTE), 
+	"RAIN_SCORE" NUMBER, 
+	"MINE_SCORE" NUMBER, 
+	"TOTAL_SCORE" NUMBER
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into ORA_USER.SCORE
+SET DEFINE OFF;
+Insert into ORA_USER.SCORE (USER_ID,RAIN_SCORE,MINE_SCORE,TOTAL_SCORE) values ('AA',0,0,0);
+Insert into ORA_USER.SCORE (USER_ID,RAIN_SCORE,MINE_SCORE,TOTAL_SCORE) values ('AAA',0,0,0);
+Insert into ORA_USER.SCORE (USER_ID,RAIN_SCORE,MINE_SCORE,TOTAL_SCORE) values ('김경래',0,0,0);
+Insert into ORA_USER.SCORE (USER_ID,RAIN_SCORE,MINE_SCORE,TOTAL_SCORE) values ('A',0,125,125);
+--------------------------------------------------------
+--  Constraints for Table SCORE
+--------------------------------------------------------
+
+  ALTER TABLE "ORA_USER"."SCORE" MODIFY ("USER_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Ref Constraints for Table SCORE
+--------------------------------------------------------
+
+  ALTER TABLE "ORA_USER"."SCORE" ADD CONSTRAINT "SCORE_FK1" FOREIGN KEY ("USER_ID")
+	  REFERENCES "ORA_USER"."USER0" ("USER_ID") ENABLE;
---
+++ SQL/WORDS.sql
@@ -0,0 +1,47 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-7월-05-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table WORDS
+--------------------------------------------------------
+
+  CREATE TABLE "ORA_USER"."WORDS" 
+   (	"WORD" VARCHAR2(20 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into ORA_USER.WORDS
+SET DEFINE OFF;
+Insert into ORA_USER.WORDS (WORD) values ('은행');
+Insert into ORA_USER.WORDS (WORD) values ('사과');
+Insert into ORA_USER.WORDS (WORD) values ('딸기');
+Insert into ORA_USER.WORDS (WORD) values ('바나나');
+Insert into ORA_USER.WORDS (WORD) values ('수박');
+Insert into ORA_USER.WORDS (WORD) values ('자몽');
+Insert into ORA_USER.WORDS (WORD) values ('귤');
+Insert into ORA_USER.WORDS (WORD) values ('멜론');
+Insert into ORA_USER.WORDS (WORD) values ('산딸기');
+Insert into ORA_USER.WORDS (WORD) values ('자두');
+Insert into ORA_USER.WORDS (WORD) values ('파인애플');
+Insert into ORA_USER.WORDS (WORD) values ('코코넛');
+Insert into ORA_USER.WORDS (WORD) values ('라임');
+Insert into ORA_USER.WORDS (WORD) values ('레몬');
+Insert into ORA_USER.WORDS (WORD) values ('석류');
+Insert into ORA_USER.WORDS (WORD) values ('배추');
+Insert into ORA_USER.WORDS (WORD) values ('머루');
+Insert into ORA_USER.WORDS (WORD) values ('망고');
+Insert into ORA_USER.WORDS (WORD) values ('딸기');
+Insert into ORA_USER.WORDS (WORD) values ('인터넷');
+Insert into ORA_USER.WORDS (WORD) values ('필통');
+Insert into ORA_USER.WORDS (WORD) values ('너와나');
+Insert into ORA_USER.WORDS (WORD) values ('커피온리일분');
+Insert into ORA_USER.WORDS (WORD) values ('강남콩');
+Insert into ORA_USER.WORDS (WORD) values ('검은콩');
+Insert into ORA_USER.WORDS (WORD) values ('딸기');
+Insert into ORA_USER.WORDS (WORD) values ('잣');
+Insert into ORA_USER.WORDS (WORD) values ('상추');
+Insert into ORA_USER.WORDS (WORD) values ('팥');
